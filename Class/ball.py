575030e106db64b2daad2fc86b012479dbc7de41
Ramazan <ramazan.du97@gmail.com>
2024-10-14 19:28:26 +0200

    add ball, and playlogic

---
+++ Class/ball.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("blue")
+        self.penup()
+        self.speed(40)
+        self.dx = 1
+        self.dy = 1
+
+    def move(self):
+        new_x = self.xcor() + self.dx
+        new_y = self.ycor() + self.dy
+        self.goto(new_x, new_y)
+
+        # Collision with walls
+        if self.ycor() > 390:
+            self.sety(390)
+            self.dy *= -1
+
+        if self.ycor() < -390:
+            self.sety(-390)
+            self.dy *= -1
+
+    def bounce_x(self):
+        self.dx *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from turtle import Screen
 import time
 from Class.paddle import Paddle
+from Class.ball import Ball
 STARTING_POSITION = [(-370,0),(+370,0)]
+
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(height=800,width=800)
 screen.title("PyPong")
+screen.tracer(0)
+
+ball = Ball()
+
+paddle_a = Paddle(STARTING_POSITION[0])
+paddle_b = Paddle(STARTING_POSITION[1])
 
 screen.listen()
+screen.onkeypress(paddle_a.up,"Up")
+screen.onkeypress(paddle_a.down,"Down")
+screen.onkeypress(paddle_b.up,"w")
+screen.onkeypress(paddle_b.down,"s")
 
-paddle = []
-for _ in range(0,2):
-    paddle.append(Paddle(STARTING_POSITION[_]))
-player = paddle[0]
-
-screen.onkeypress(player.up,"Up")
-screen.onkeypress(player.down,"Down")
 
 game_is_on =  True
-while not game_is_on:
-    time.sleep(0.1)
+while game_is_on:
+    screen.update()
+    time.sleep(0.01)  # Adjust speed as necessary
 
+    ball.move()
 
+    if ball.xcor() < -360 and paddle_a.ycor() - 50 < ball.ycor() < paddle_a.ycor() + 50:
+        ball.bounce_x()
+
+    if ball.xcor() > 360 and paddle_b.ycor() - 50 < ball.ycor() < paddle_b.ycor() + 50:
+        ball.bounce_x()
+
+    # Detect if ball goes out of bounds
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()
